dd6c959624165e4b6fa28b0635a1e766690592a2
joemanix65 <joe_manix@msn.com>
2021-08-04 21:26:11 +0200

    Finalisation page produit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 JosePerez_5_22072021
 cmd:    cd back-end
         node server.js
+
+ 
--- front-end/js/produit.js
+++ front-end/js/produit.js
@@ -12,26 +12,27 @@ fetch(`http://localhost:3000/api/cameras/${leId}`)
         response.json()
     .then(jsonProduct => {
             console.log(jsonProduct);
-            let affichage = "";
-            
-                affichage += `<div class="img-container-produit">
-                <img class="img-cam" src="${jsonProduct.imageUrl}" alt="image de la caméra">
-                <div class="card-text">
-                    <h3 class="card-titre">${jsonProduct.name}</h3>
-                    <p class="card-description">${jsonProduct.description}</p>
-                    <p class="card-prix-produit">${jsonProduct.price/100},00€</p>
-                    <p class="input-text">Type de lentille:
-                    <select name="lentille-type" id="input1"><option value="35mm">${jsonProduct.lenses[0]}</option><option value="35mm">${jsonProduct.lenses[1]}</option><option value="35mm">${jsonProduct.lenses[2]}</option></select></p>
-                    <p class="input-text">Quantité:
-                    <input id="input2" type="number" name="tentacles" min="1" max="10" value="1"></p>
-                    <div class="button">
-                        <a class="button1" href="../index.html">Retour Accueil</a>
-                        <a class="button2" href="">Acheter</a>
+        let affichage = "";
+            affichage += `<div class="img-container-produit">
+                            <img class="img-cam" src="${jsonProduct.imageUrl}" alt="image de la caméra">
+                        <div class="card-text">
+                        <h3 class="card-titre">${jsonProduct.name}</h3>
+                        <p class="card-description">${jsonProduct.description}</p>
+                        <p class="card-prix-produit">${jsonProduct.price/100},00€</p>
+                        <p class="input-text">Type de lentille:
+                        <select name="lentille-type" id="input1"></select></p>
+                        <p class="input-text">Quantité:
+                        <input id="input2" type="number" name="tentacles" min="1" max="10" value="1"></p>
+                        <div class="button">
+                            <a class="button1" href="../index.html">Retour Accueil</a>
+                            <a class="button2" href="">Acheter</a>
+                        </div>
                     </div>
-                </div>
-                
-            </div>`
+                </div>`
             document.querySelector('#produit').innerHTML += affichage
-            }))
-            
-              
+           
+            for (let _lenses of jsonProduct.lenses){
+                console.log(_lenses)
+                document.querySelector('#input1').innerHTML += `<option value="35mm">${_lenses}</option>`;
+            };
+        }))
